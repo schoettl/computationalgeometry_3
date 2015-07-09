d4bfb8bf759cc60bb1d7335a3a7ac01418f7bd43
Jakob Schöttl <jschoett@gmail.com>
2015-07-09 14:31:05 +0200

    Removed old files

--- test-c-vs-h/make-dat-files-for-R.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-<"$1" awk '/./ && NR>1' \
-    | awk 'NF==2;NF==1{print "intersects " $1}' \
-    | awk '{if($0~/^processing/) {print $0; printf "lines "; system("wc -l <../" $2)} else print}' \
-    | awk '{if($0~/^(real|user|sys)/){split($2, arr, "m"); print $1 FS (arr[1]*60 + arr[2])} else print}' \
-    | awk '{print int((NR-1)/6)+1 FS $0}' >"$2"
--- test-haskell-with-nubBy/script.awk
+++
@@ -1,2 +0,0 @@
-awk 'BEGIN{FS=";"}; {split($2, arr, "m"); print $1 FS $2 FS (arr[1]*60 + arr[2])}' haskell-times.csv > haskell-times-sec.csv
-
